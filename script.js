20d26778f9860facf427a72d75470dfe50093a79
ZhengYeah <zhengyeah@foxmail.com>
2025-12-18 18:40:58 -0500

    Add preprints page

--- script.js
+++ script.js
@@ -77,35 +77,41 @@ window.addEventListener("resize", () => {
 /*
 Profile image carousel: when the profile image is clicked, change to the next image.
 */
-const totalImgNum = 3; // only change this number when adding more images
+// if not home page, do not run the profile image carousel script
+if (!document.getElementById("showing-profile")) {
+    // do nothing
+} else {
+    const totalImgNum = 3; // only change this number when adding more images
 
 // change profile image when clicked (PC)
-const showingImg = document.getElementById("showing-profile");
-let i = 1;
-showingImg.addEventListener("click", () => {
-    i = i % totalImgNum + 1;
-    let imgPath = "./images/profile/profile-" + i + ".jpg";
-    showingImg.setAttribute("src", imgPath);
-})
+    const showingImg = document.getElementById("showing-profile");
+    let i = 1;
+    showingImg.addEventListener("click", () => {
+        i = i % totalImgNum + 1;
+        let imgPath = "./images/profile/profile-" + i + ".jpg";
+        showingImg.setAttribute("src", imgPath);
+    })
 
 // change profile image when the img and buttons are clicked (mobile)
-const showingMobileImg = document.getElementById("showing-mobile-profile");
-let j = 1;
-showingMobileImg.addEventListener("click", () => {
-    j = j % totalImgNum + 1;
-    let imgPath = "./images/profile/profile-" + j + ".jpg";
-    showingMobileImg.setAttribute("src", imgPath);
-})
+    const showingMobileImg = document.getElementById("showing-mobile-profile");
+    let j = 1;
+    showingMobileImg.addEventListener("click", () => {
+        j = j % totalImgNum + 1;
+        let imgPath = "./images/profile/profile-" + j + ".jpg";
+        showingMobileImg.setAttribute("src", imgPath);
+    })
 // left and right buttons for mobile
-const leftButton = document.getElementById("profile-left-button");
-const rightButton = document.getElementById("profile-right-button");
-leftButton.addEventListener("click", () => {
-    j = (j - 2 + totalImgNum) % totalImgNum + 1;
-    let imgPath = "./images/profile/profile-" + j + ".jpg";
-    showingMobileImg.setAttribute("src", imgPath);
-})
-rightButton.addEventListener("click", () => {
-    j = j % totalImgNum + 1;
-    let imgPath = "./images/profile/profile-" + j + ".jpg";
-    showingMobileImg.setAttribute("src", imgPath);
-})
+    const leftButton = document.getElementById("profile-left-button");
+    const rightButton = document.getElementById("profile-right-button");
+    leftButton.addEventListener("click", () => {
+        j = (j - 2 + totalImgNum) % totalImgNum + 1;
+        let imgPath = "./images/profile/profile-" + j + ".jpg";
+        showingMobileImg.setAttribute("src", imgPath);
+    })
+    rightButton.addEventListener("click", () => {
+        j = j % totalImgNum + 1;
+        let imgPath = "./images/profile/profile-" + j + ".jpg";
+        showingMobileImg.setAttribute("src", imgPath);
+    })
+}
+
